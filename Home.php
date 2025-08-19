cd3768ae829f456c0587495b7acac96525b5c176
nondrack <luckasfernando675@gmail.com>
2025-08-18 21:17:45 -0300

    atualização

--- Home.php
+++ Home.php
@@ -1,14 +1,47 @@
-
+<?php
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Compras</title>
+    <title>Sistema NFC-e</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="cabeçario.css">
 </head>
-<body>
-    
-<h1> teste </h1>
 
+<body>
+    <!-- ============== Cabeçalho ========= -->
+    <header class="navbar navbar-expand-lg shadow">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">Sistema NFC-e</a>
+            <!--================Menu====================-->
+            <div class="collapse navbar-collapse" class="menu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Notas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Fornecedores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Relatórios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-light btn-sm ms-2" href="#">Sair</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
+
+
